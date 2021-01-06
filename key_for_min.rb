8d42576347ffbf5069776f8ea5d1b8a635da0864
Samuel Ferebee <sam.ferebee@gmail.com>
2021-01-06 21:21:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,12 @@
 
 def key_for_min_value(name_hash)
   smallest = 100000
+  the_key
   name_hash.each do |key, value|
     if value < smallest
       smallest = value
+      the_key = key
     end
   end
+  the_key
 end
